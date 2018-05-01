ea0ed70595278e4efa7d47d68a21e213d2cb44a3
Alan Neri <alanneri10@gmail.com>
2018-05-01 15:09:14 -0500

    Fix issue Context Null - Android

--- src/conekta.android.ts
+++ src/conekta.android.ts
@@ -2,14 +2,16 @@ import * as app from "tns-core-modules/application";
 
 import { Common } from './conekta.common';
 
-let activity = app.android.startActivity || app.android.foregroundActivity;
 declare const io, org: any;
 
 export class Conekta extends Common {
 
     createToken(name: string, numberCard: string, cvc: string, expMonth: string, expYear: string, publicKey: string): Promise<string> {   
         return new Promise(function (resolve, reject) {
+
+            let activity = app.android.startActivity || app.android.foregroundActivity;
             let mToken: string = null;
+
             io.conekta.conektasdk.Conekta.setPublicKey(publicKey);
             io.conekta.conektasdk.Conekta.setApiVersion("1.0.0");
             io.conekta.conektasdk.Conekta.collectDevice(activity); 
